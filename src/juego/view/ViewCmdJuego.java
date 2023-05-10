38ac0d6221c8fa6b1f99fbf391b685f1a7974a10
SrHuevo04 <115703280+SrHuevo04@users.noreply.github.com>
2023-05-10 11:55:22 +0200

    Add files via upload

--- src/juego/view/ViewCmdJuego.java
+++ src/juego/view/ViewCmdJuego.java
@@ -14,6 +14,7 @@ import jugador.model.Jugador;
 
 public class ViewCmdJuego {
 	private IRepoJuego juegos;
+	private Scanner scanner;
 	public ViewCmdJuego() {
 		juegos = new RepoFileJuego();
 	}
@@ -21,7 +22,7 @@ public class ViewCmdJuego {
 		boolean salir = true;
 
 
-		Scanner scanner = new Scanner(System.in);
+		scanner = new Scanner(System.in);
 		while (salir) {
 
 			System.out.println("--------------------");
@@ -38,86 +39,8 @@ public class ViewCmdJuego {
 
 
 			case "1":
-				Scanner scanner2 = new Scanner(System.in);
-				System.out.println("¿Quieres un juego predefinido o crear el tuyo propio?");
-				System.out.println("1. Predefinido 2. Propio"); 
-				int respuesta = scanner2.nextInt();
-				switch(respuesta) {
-
-				case 1:
-					System.out.println("1. Cuadrado  2. Rectangular");
-					int respuesta2 = scanner2.nextInt();
-					if (respuesta2 == 1) {
-						System.out.println("Introduce tu nombre: ");
-						String nombre = scanner.next();
-						System.out.println("Introduce tu expediente: ");
-						int expediente = scanner.nextInt();
-						System.out.println("Introduce eln nombre de tu juego: ");
-						String nombreJuego = scanner.next();
-						JuegoCuadPredefinido juegocuad = new JuegoCuadPredefinido(0,0,new Jugador(nombre,expediente),nombreJuego);
-						System.out.println("1. 5x5  2. 10x10  3. 20x20");
-						int respuestaCuad = scanner2.nextInt();
-						switch(respuestaCuad) {
-						case 1:
-							juegocuad.setJuego5();
-							juegos.create(juegocuad);
-							break;
-						case 2:
-							juegocuad.setJuego10();
-							juegos.create(juegocuad);
-							break;
-						case 3:
-							juegocuad.setJuego20();
-							juegos.create(juegocuad);
-							break;
-						}
-					}
-					else if(respuesta2 ==2) {
-						System.out.println("Introduce tu nombre: ");
-						String nombre = scanner.next();
-						System.out.println("Introduce tu expediente: ");
-						int expediente = scanner.nextInt();
-						System.out.println("Introduce eln nombre de tu juego: ");
-						String nombreJuego = scanner.next();
-						JuegoRectPredefinido juegorect = new JuegoRectPredefinido(0,0,new Jugador(nombre,expediente),nombreJuego);
-						System.out.println("1. 10x5  2. 20x10  3. 30x15");
-						int respuestaRect = scanner2.nextInt();
-						switch(respuestaRect) {
-						case 1:
-							juegorect.setJuego10x5();
-							juegos.create(juegorect);
-							break;
-						case 2:
-							juegorect.setJuego20x10();
-							juegos.create(juegorect);
-							break;
-						case 3:
-							juegorect.setJuego30x15();
-							juegos.create(juegorect);
-							break;
-						}
-					}
-					else {
-						System.out.println("Respuesta erronea");
-					}
-					break;
-					default:
-						System.out.println("Respuesta erronea");
-				case 2:
-					System.out.println("Introduce tu nombre: ");
-					String nombre = scanner.next();
-					System.out.println("Introduce tu expediente: ");
-					int expediente = scanner.nextInt();
-					System.out.println("Introduce eln nombre de tu juego: ");
-					String nombreJuego = scanner.next();
-					System.out.println("Introduce la dimensión del eje x que quieras: ");
-					int dimX = scanner.nextInt();
-					System.out.println("Introduce la dimensión del eje y que quieras: ");
-					int dimY = scanner.nextInt();
-					Juego juego = new Juego(dimX,dimY,new Jugador(nombre,expediente),nombreJuego);
-					juegos.create(juego);
-					break;
-				}
+				ViewCmdCreateJuego vccj = new ViewCmdCreateJuego(juegos, scanner);
+				vccj.menu();
 				break;
 			case "2":
 				List<Juego> games = juegos.read();
@@ -144,7 +67,7 @@ public class ViewCmdJuego {
 
 
 			case "4":
-				System.out.println("Introduce el nombre del : ");
+				System.out.println("Introduce el nombre del juego: ");
 				String nombreJuegoToDelete = scanner.next();
 				int juegoToDelete = juegos.buscar(nombreJuegoToDelete);
 				if ( juegoToDelete == -1) {
@@ -155,7 +78,6 @@ public class ViewCmdJuego {
 					System.out.println("Se ha eliminado correctamente");
 				}
 				break;
-
 			case "5":
 				salir = false;
 				break;
