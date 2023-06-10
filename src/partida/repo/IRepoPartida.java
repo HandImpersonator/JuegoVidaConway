5f70682910d39cd1dd3dd5b099e3031bd4b538f8
HandImpersonator <96979322+HandImpersonator@users.noreply.github.com>
2023-06-11 01:13:43 +0200

    Add files via upload

--- src/partida/repo/IRepoPartida.java
+++ src/partida/repo/IRepoPartida.java
@@ -1,3 +1,11 @@
+/**
+ * Interfaz para el repositorio de partidas.
+ * Esta interfaz define los métodos necesarios para crear, leer, actualizar y eliminar partidas,
+ * así como realizar otras operaciones relacionadas.
+ *
+ * @author User
+ */
+
 package partida.repo;
 
 import java.util.List;
@@ -6,11 +14,54 @@ import partida.model.Partida;
 
 
 public interface IRepoPartida {
-    public void create(Partida partida);
-    public List<Partida> read(int idExp);
-    public List<Partida> readFile(String ruta);
-    public boolean update(int idExp, int posx, Partida partida);
-    public boolean remove(int idExp, int posx);
-    public int buscar(int idE, int id);
-    public int size();
+    /**
+     * Crea una nueva partida.
+     *
+     * @param partida La partida a crear.
+     */
+    void create(Partida partida);
+
+    /**
+     * Lee las partidas asociadas a un ID de expediente.
+     *
+     * @param idExp El ID de expediente para el cual se desean obtener las partidas.
+     * @return Una lista de partidas asociadas al ID de expediente.
+     */
+    List<Partida> read(int idExp);
+
+    /**
+     * Actualiza una partida existente.
+     *
+     * @param idExp El ID de expediente al que pertenece la partida.
+     * @param posx La posición de la partida en la lista de partidas.
+     * @param partida La partida actualizada.
+     * @return true si la actualización fue exitosa, false de lo contrario.
+     */
+    boolean update(int idExp, int posx, Partida partida);
+
+    /**
+     * Elimina una partida existente.
+     *
+     * @param idExp El ID de expediente al que pertenece la partida.
+     * @param posx La posición de la partida en la lista de partidas.
+     * @return true si la eliminación fue exitosa, false de lo contrario.
+     */
+    boolean remove(int idExp, int posx);
+
+    /**
+     * Busca una partida por su ID de expediente y su ID de partida.
+     *
+     * @param idE El ID de expediente de la partida a buscar.
+     * @param id El ID de partida a buscar.
+     * @return La posición de la partida en la lista de partidas, o -1 si no se encontró la partida.
+     */
+    int buscar(int idE, int id);
+
+    /**
+     * Obtiene el número total de partidas almacenadas.
+     *
+     * @return El número total de partidas almacenadas.
+     */
+    int size();
+
 }
--- src/partida/repo/RepoFileBinPartida.java
+++ src/partida/repo/RepoFileBinPartida.java
@@ -1,88 +1,75 @@
+/**
+ * Clase que crea nuevas partidas, lee partidas, lee ficheros de partidas, actualiza un fichero de partida y borra una partida de un fichero.
+ * Esta clase proporciona métodos para realizar operaciones matemáticas simples.
+ *
+ * @author User
+ */
+
 package partida.repo;
 
-import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import common.util.FileUtil;
 import partida.model.Partida;
 
-
 public class RepoFileBinPartida implements IRepoPartida {
     private List<Partida> partidas;
+    private Path dataDirectory;
 
     public RepoFileBinPartida() {
         this.partidas = new ArrayList<>();
+        this.dataDirectory = Paths.get("data");
     }
 
     @Override
     public void create(Partida partida) {
         partidas.add(partida);
-        String ruta = "data/partida_" + String.valueOf(partida.getIdExp()) + ".dat";
-        FileUtil.serializeFromListToFile(ruta, partidas);
+        Path filePath = getFilePath(partida.getIdExp());
+        FileUtil.serializeFromListToFile(filePath.toString(), partidas);
     }
 
     @Override
     public List<Partida> read(int idExp) {
-        File[] fileLista = (new File("data/").listFiles());
-        for (File element : fileLista) {
-            if (!element.isDirectory()) {
-                String nameFile = element.getName();
-                String compareFile = "partida_" + String.valueOf(idExp) + ".dat";
-                if (nameFile.equals(compareFile)) {
-                    String ruta = "data/" + nameFile;
-                    partidas = FileUtil.deserializeFromFileToList(ruta);
-                    return partidas;
-                }
-            }
-        }
-        return partidas;
-    }
-    
-    public List<Partida> readFile(String ruta) {
-        partidas = FileUtil.deserializeFromFileToList(ruta);
+        Path filePath = getFilePath(idExp);
+        partidas = FileUtil.deserializeFromFileToList(filePath.toString());
         return partidas;
     }
 
     @Override
     public boolean update(int idExp, int posx, Partida Partida) {
-        String ruta = "data/partida_" + String.valueOf(idExp) + ".dat";
-        List<Partida> parts = readFile(ruta);
+        Path filePath = getFilePath(idExp);
+        List<Partida> parts = read(idExp);
         if ((posx < 0) || (posx >= parts.size())) {
             return false;
         } else {
             parts.set(posx, Partida);
-            FileUtil.serializeFromListToFile(ruta, parts);
+            FileUtil.serializeFromListToFile(filePath.toString(), parts);
             return true;
         }
     }
 
     @Override
     public boolean remove(int idExp, int posx) {
-        String ruta = "data/partida_" + String.valueOf(idExp) + ".dat";
-        List<Partida> parts = readFile(ruta);
+        Path filePath = getFilePath(idExp);
+        List<Partida> parts = read(idExp);
         if ((posx < 0) || (posx >= parts.size())) {
             return false;
         } else {
             parts.remove(posx);
-            FileUtil.serializeFromListToFile(ruta, parts);
+            FileUtil.serializeFromListToFile(filePath.toString(), parts);
             return true;
         }
     }
 
     @Override
     public int buscar(int idExp, int id) {
-        File[] fileLista = (new File("data/").listFiles());
-        for (File element : fileLista) {
-            if (!element.isDirectory()) {
-                String nameFile = element.getName();
-                String compareFile = "partida_" + String.valueOf(idExp) + ".dat";
-                if (nameFile.equals(compareFile)) {
-                    String ruta = "data/" + nameFile;
-                }
-            }
-        }
-        for (int i = 0; i < partidas.size(); i++) {
+        Path filePath = getFilePath(idExp);
+        partidas = FileUtil.deserializeFromFileToList(filePath.toString());
+        for (int i = 0; i < Objects.requireNonNull(partidas).size(); i++) {
             if (partidas.get(i).equals(new Partida(id))) {
                 return i;
             }
@@ -93,5 +80,10 @@ public class RepoFileBinPartida implements IRepoPartida {
     @Override
     public int size() {
         return partidas.size();
+    }
+
+    private Path getFilePath(int idExp) {
+        String fileName = "partida_" + String.valueOf(idExp) + ".dat";
+        return dataDirectory.resolve(fileName);
     }
 }
