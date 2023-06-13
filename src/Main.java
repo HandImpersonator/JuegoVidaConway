1119b41564c02da8a805194d75813425e19fdae1
HandImpersonator <96979322+HandImpersonator@users.noreply.github.com>
2023-06-13 21:59:49 +0200

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import jugador.view.ViewWindowLoginJugador;
-
-public class Main {
-
-    public static void main(String[] args) {
-        ViewWindowLoginJugador view = new ViewWindowLoginJugador();
-        view.setVisible(true);
-    }
-}
